d36909797a932657abba0019f6785581b9a976b1
jonghun <minn4072@gmail.com>
2023-08-26 16:01:17 +0900

    feat: ormConfig 연결

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ormConfig } from 'orm.config';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRootAsync({
+      useFactory: ormConfig,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
