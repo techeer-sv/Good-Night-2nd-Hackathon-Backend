f9c7a421e03ace4b15677a6adec64dc37738594f
choiseyeon <choi20014830@gmail.com>
2023-08-26 20:28:58 +0900

    feat: DB 연결 완료

--- main.go
+++ main.go
@@ -1,11 +1,21 @@
-package Good_Night_2nd_Hackathon_Backend
+package main
 
 import (
+	"fmt"
 	"github.com/gofiber/fiber/v2"
 	"log"
+	"server/database"
 )
 
 func main() {
+	// Connect with database
+	db, err := database.DatabaseConnection()
+
+	if err != nil {
+		log.Fatal("Database Connection Error: ", err)
+	}
+	fmt.Println("Database connection success!:", db)
+
 	app := fiber.New()
 
 	app.Get("/ping", func(c *fiber.Ctx) error {
