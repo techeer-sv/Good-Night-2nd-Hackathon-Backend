4794cb4d8a5a741e62ad31b6ce3e4bb251abe8d5
hangyeol <snake011128@naver.com>
2023-08-26 14:57:37 +0900

    movie delete(soft) API 구현

--- movie/controller.go
+++ movie/controller.go
@@ -4,6 +4,7 @@ import (
 	"Good-Night-2nd-Hackathon-Backend/config/database"
 	"github.com/gin-gonic/gin"
 	"net/http"
+	"time"
 )
 
 var db = database.GetDB()
@@ -12,6 +13,7 @@ func Config(api *gin.RouterGroup) {
 	_ = db.DB.AutoMigrate(&Movie{})
 	api.POST("/movies", createMovie)
 	api.GET("/movies/:id", getMovie)
+	api.DELETE("/movies/:id", deleteMovie)
 }
 
 func createMovie(c *gin.Context) {
@@ -42,4 +44,21 @@ func getMovie(c *gin.Context) {
 	}
 
 	c.JSON(http.StatusOK, movie.fromEntity())
+}
+
+func deleteMovie(c *gin.Context) {
+	movieID := c.Param("id")
+
+	var movie Movie
+	if err := db.DB.First(&movie, movieID).Error; err != nil {
+		c.AbortWithStatusJSON(http.StatusNotFound, gin.H{"error": "영화를 찾을 수 없습니다."})
+		return
+	}
+
+	if err := db.DB.Model(&movie).Update("deleted_at", time.Now()).Error; err != nil {
+		c.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{"error": "영화 삭제에 실패하였습니다."})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{"error": "영화가 삭제되었습니다."})
 }
