acf8ae5b827caae1857fc6dea4564aea67488055
choiseyeon <choi20014830@gmail.com>
2023-08-26 20:13:40 +0900

    fix: 스키마 수정

--- database/migrations/schema.sql
+++ database/migrations/schema.sql
@@ -1,22 +1,22 @@
 CREATE TABLE movies (
-    id INT AUTO_INCREMENT PRIMARY KEY,
+    id SERIAL PRIMARY KEY,
     title VARCHAR(255) NOT NULL,
-    genre ENUM('스릴러', '로맨스', '코믹', '액션') NOT NULL,
+    genre VARCHAR(10) CHECK (genre IN ('스릴러', '로맨스', '코믹', '액션')) NOT NULL,
     release_date DATE,
     end_date DATE,
     is_showing BOOLEAN,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-    deleted_at TIMESTAMP NULL
+    created_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP,
+    deleted_at TIMESTAMPTZ NULL
 );
 
 CREATE TABLE reviews (
-     id INT AUTO_INCREMENT PRIMARY KEY,
+     id SERIAL PRIMARY KEY,
      movie_id INT,
      rating INT CHECK (rating >= 1 AND rating <= 5),
      content TEXT,
-     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-     deleted_at TIMESTAMP NULL
+     created_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP,
+     updated_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP,
+     deleted_at TIMESTAMPTZ NULL,
      FOREIGN KEY (movie_id) REFERENCES movies(id)
 );
