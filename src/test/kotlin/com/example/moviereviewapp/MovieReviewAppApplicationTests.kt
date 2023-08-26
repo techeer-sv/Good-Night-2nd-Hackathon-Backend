4a16e78d580475b98212a20c88f1718080dbfcec
non <dojeon218@gmail.com>
2023-08-26 12:18:28 +0900

    init

---
+++ src/test/kotlin/com/example/moviereviewapp/MovieReviewAppApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.moviereviewapp
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MovieReviewAppApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
