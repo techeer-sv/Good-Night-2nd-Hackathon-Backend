48d8e20b24e3f6c38ca534ee87f460ad4a546be6
hangyeol <snake011128@naver.com>
2023-08-26 16:41:17 +0900

    리뷰 목록 API 구현

--- review/controller.go
+++ review/controller.go
@@ -4,6 +4,7 @@ import (
 	"Good-Night-2nd-Hackathon-Backend/config/database"
 	"github.com/gin-gonic/gin"
 	"net/http"
+	"strconv"
 )
 
 var db = database.GetDB()
@@ -11,6 +12,7 @@ var db = database.GetDB()
 func Config(api *gin.RouterGroup) {
 	_ = db.DB.AutoMigrate(&Review{})
 	api.POST("/reviews", createReview)
+	api.GET("/reviews/:id", getReviews)
 }
 
 func createReview(c *gin.Context) {
@@ -28,4 +30,33 @@ func createReview(c *gin.Context) {
 	}
 
 	c.JSON(http.StatusCreated, r.fromEntity())
+}
+
+func getReviews(c *gin.Context) {
+	movieID := c.Param("id")
+
+	var reviews []Review
+	query := db.DB.Order("created_at DESC")
+
+	query = query.Where("movie_id = ?", movieID)
+
+	rating := c.Query("rating")
+	if rating != "" {
+		rating, err := strconv.ParseFloat(rating, 64)
+		if err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": "유효하지 않은 값입니다."})
+			return
+		}
+		query = query.Where("rating >= ?", rating)
+	}
+
+	query.Find(&reviews)
+
+	var response []response
+
+	for _, review := range reviews {
+		response = append(response, review.fromEntity())
+	}
+
+	c.JSON(http.StatusOK, response)
 }
