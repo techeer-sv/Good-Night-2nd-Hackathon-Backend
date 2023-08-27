1543498ab9d3024d83631149bb43a59a6c62af60
Mayreeel <kuwon15@gmail.com>
2023-08-27 23:16:39 +0900

    feat : 트랜잭션 추가

--- src/review/review.service.ts
+++ src/review/review.service.ts
@@ -1,10 +1,12 @@
-import { Inject, Injectable, NotFoundException } from '@nestjs/common';
+import { Inject, Injectable, Logger, NotFoundException } from '@nestjs/common';
 import { Repository } from 'typeorm';
 import { Review } from './review.entity';
 import { Movie } from '../movie/movie.entity';
 
 @Injectable()
 export class ReviewService {
+  private readonly logger = new Logger(ReviewService.name);
+
   constructor(
     @Inject('REVIEW_REPOSITORY')
     private reviewRepository: Repository<Review>,
@@ -17,25 +19,48 @@ export class ReviewService {
     rating: number,
     content: string,
   ): Promise<Review> {
-    const movie = await this.movieRepository.findOne({
-      where: { id: movieId },
-      relations: ['reviews'],
-    });
-    if (!movie) {
-      throw new NotFoundException('Movie not found');
+    const queryRunner =
+      this.reviewRepository.manager.connection.createQueryRunner();
+    await queryRunner.connect();
+    await queryRunner.startTransaction();
+
+    try {
+      const movie = await this.movieRepository.findOne({
+        where: { id: movieId },
+        relations: ['reviews'],
+      });
+      if (!movie) {
+        throw new NotFoundException('Movie not found');
+      }
+
+      const review = this.reviewRepository.create({ rating, content, movie });
+      const newReview = await queryRunner.manager.save(Review, review);
+
+      const totalRating = movie.reviews.reduce(
+        (sum, review) => sum + review.rating,
+        0,
+      );
+      if (movie.reviews.length === 0) {
+        movie.averageRating = rating;
+      } else {
+        movie.averageRating = totalRating / movie.reviews.length;
+      }
+
+      movie.reviews.push(newReview);
+
+      await queryRunner.manager.save(Movie, movie);
+
+      await queryRunner.commitTransaction();
+
+      this.logger.log(`Review created successfully: ${newReview.id}`);
+      return newReview;
+    } catch (error) {
+      await queryRunner.rollbackTransaction();
+      this.logger.error(`Error creating review: ${error.message}`);
+      throw error;
+    } finally {
+      await queryRunner.release();
     }
-
-    const review = this.reviewRepository.create({ rating, content, movie });
-    const newReview = await this.reviewRepository.save(review);
-
-    const totalRating = movie.reviews.reduce(
-      (sum, review) => sum + review.rating,
-      0,
-    );
-    movie.averageRating = totalRating / movie.reviews.length;
-    await this.movieRepository.save(movie);
-
-    return newReview;
   }
 
   async getMovieReviews(
