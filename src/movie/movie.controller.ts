17193092d8a1cf0e1dffeaf93aff1fdffa876d40
suhyeon <suhyeon3484@naver.com>
2023-09-02 04:41:14 +0900

    feat: Add swagger

--- src/movie/movie.controller.ts
+++ src/movie/movie.controller.ts
@@ -12,17 +12,21 @@ import { MovieService } from './movie.service';
 import { CreateMovieDto } from './dto/create-movie.dto';
 import { Movie } from './entity/movie.entiity';
 import { UpdateMovieDto } from './dto/update-movie.dto';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
 
+@ApiTags('Movie')
 @Controller('movie')
 export class MovieController {
   constructor(private readonly movieService: MovieService) {}
 
   @Post()
+  @ApiOperation({ summary: '영화 생성' })
   async create(@Body() createMovieDto: CreateMovieDto) {
     return await this.movieService.createMovie(createMovieDto);
   }
 
   @Get()
+  @ApiOperation({ summary: '영화 목록 조회' })
   async findAll(
     @Query('genre') genre: string,
     @Query('isShowing') isShowing: boolean,
@@ -31,11 +35,13 @@ export class MovieController {
   }
 
   @Get(':id')
+  @ApiOperation({ summary: '영화 상세 조회' })
   async findOne(@Param('id') id: number): Promise<Movie> {
     return await this.movieService.findOne(id);
   }
 
   @Put(':id')
+  @ApiOperation({ summary: '영화 수정' })
   async update(
     @Param('id') id: number,
     @Body() updateMovieDto: UpdateMovieDto,
@@ -44,6 +50,7 @@ export class MovieController {
   }
 
   @Delete(':id')
+  @ApiOperation({ summary: '영화 삭제' })
   async delete(@Param('id') id: number): Promise<void> {
     await this.movieService.delete(id);
   }
--- src/review/review.controller.ts
+++ src/review/review.controller.ts
@@ -2,18 +2,22 @@ import { Body, Controller, Get, HttpCode, Param, Post } from '@nestjs/common';
 import { ReviewService } from './review.service';
 import { CreateReviewDto } from './dto/create-review.dto';
 import { Review } from './entity/review.entity';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
 
+@ApiTags('Review')
 @Controller('review')
 export class ReviewController {
   constructor(private reviewService: ReviewService) {}
 
   @Post()
+  @ApiOperation({ summary: '리뷰 생성' })
   @HttpCode(200)
   async create(@Body() createReviewDto: CreateReviewDto): Promise<void> {
     await this.reviewService.create(createReviewDto);
   }
 
   @Get('/:movieId')
+  @ApiOperation({ summary: '리뷰 목록 조회' })
   async findReviewsByMovieId(
     @Param('movieId') movieId: number,
   ): Promise<Review[]> {
